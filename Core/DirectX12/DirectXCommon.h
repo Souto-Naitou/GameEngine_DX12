2581424817c59ba1bed3301fd028238a648a20d6
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-10-08 14:06:42 +0900

    リークチェッカを導入

--- Core/DirectX12/DirectXCommon.h
+++ Core/DirectX12/DirectXCommon.h
@@ -10,6 +10,7 @@
 #include <memory>
 
 #include <PipelineState.h>
+#include "ReakChecker.h"
 
 class DirectXCommon
 {
@@ -19,7 +20,12 @@ public:
     DirectXCommon& operator=(const DirectXCommon&) = delete;
     DirectXCommon& operator=(const DirectXCommon&&) = delete;
 
-    static DirectXCommon* GetInstance() { static DirectXCommon instance; return &instance;}
+    static DirectXCommon* GetInstance()
+    { 
+        static D3DResourceLeakChecker leakchecker;
+        static DirectXCommon instance; 
+        return &instance;
+    }
 
     /// <summary>
     /// 初期化
@@ -45,21 +51,17 @@ private:
     Microsoft::WRL::ComPtr<IDXGIFactory7>               dxgiFactory_                    = nullptr;      // DXGIファクトリ
     Microsoft::WRL::ComPtr<IDXGIAdapter4>               useAdapter_                     = nullptr;      // 使うアダプタ
     Microsoft::WRL::ComPtr<ID3D12Device>                device_                         = nullptr;      // デバイス
-    Microsoft::WRL::ComPtr<ID3D12InfoQueue>             infoQueue_                      = nullptr;      
+    Microsoft::WRL::ComPtr<ID3D12InfoQueue>             infoQueue_                      = nullptr;
     Microsoft::WRL::ComPtr<ID3D12CommandQueue>          commandQueue_                   = nullptr;      // コマンドキュー
     Microsoft::WRL::ComPtr<ID3D12CommandAllocator>      commandAllocator_               = nullptr;      // コマンドアロケータ
     Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList>   commandList_                    = nullptr;      // コマンドアロケータ
     Microsoft::WRL::ComPtr<IDXGISwapChain4>             swapChain_                      = nullptr;      // スワップチェーン
     Microsoft::WRL::ComPtr<ID3D12Resource>              swapChainResources[2]           = {};           // スワップチェーンリソース
     Microsoft::WRL::ComPtr<ID3D12Fence>                 fence_                          = nullptr;      // フェンス
-    Microsoft::WRL::ComPtr<ID3D12RootSignature>         rootSignature_                  = nullptr;      // ルート署名
     Microsoft::WRL::ComPtr<ID3D12Resource>              depthStencilResource_           = nullptr;
     Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>        rtvDescriptorHeap_              = nullptr;
     Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>        srvDescriptorHeap_              = nullptr;
-    Microsoft::WRL::ComPtr<ID3DBlob>                    signatureBlob_                  = nullptr;
-    Microsoft::WRL::ComPtr<ID3DBlob>                    errorBlob_                      = nullptr;
     Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>        dsvDescriptorHeap_              = nullptr;
-    D3D12_ROOT_PARAMETER                                rootParameters_[4]              = {};
     D3D12_COMMAND_QUEUE_DESC                            commandQueueDesc_               = {};           // コマンドキューの設定
     DXGI_SWAP_CHAIN_DESC1                               swapChainDesc_                  = {};           // スワップチェーンの設定
     D3D12_RENDER_TARGET_VIEW_DESC                       rtvDesc_                        = {};           // RTVの設定
