87e54614417575b052f9032d0d51ed4834dd60a8
Souto-Naitou <k023g0031@g.neec.ac.jp>
2025-05-22 10:14:23 +0900

    loaderの実装中

--- Engine/Features/LevelLoader/LevelLoader.cpp
+++ Engine/Features/LevelLoader/LevelLoader.cpp
@@ -1,6 +1,59 @@
 #include "LevelLoader.h"
+#include <fstream>
 
 void LevelLoader::LoadLevel(const std::string& _filename)
 {
+    json jsondata;
 
+    LoadJson(_filename, jsondata);
+
+
+}
+
+void LevelLoader::LoadJson(const std::string& _filename, json& _json)
+{
+    const std::string fullpath = directory_ + _filename + extension_;
+
+    std::fstream file(fullpath);
+
+    if (file.is_open() == false)
+    {
+        // ファイル開けてない
+        __debugbreak;
+    };
+
+    file >> _json;
+
+    file.close();
+}
+
+void LevelLoader::DeserializeJsonData(json& _json)
+{
+    std::string jsonname = _json["name"].get<std::string>();
+    if (jsonname != "scene") __debugbreak;
+
+    for (json& object : _json["objects"])
+    {
+        if (object != "type") __debugbreak;
+
+        // 種別を取得
+        std::string type = object["type"].get<std::string>();
+
+        // 種類ごとの処理
+        if (type == "MESH")
+        {
+            levelData_->objects.emplace_back(LevelData::ObjectData{});
+            auto& objectData = levelData_->objects.back();
+
+
+        }
+    }
+}
+
+void LevelLoader::DeserialiceMeshData(json& _object, LevelData::ObjectData& _objectData)
+{
+    if (_object == "file_name")
+    {
+        _objectData.filename = _object["file_name"];
+    }
 }
--- Engine/Features/LevelLoader/LevelLoader.h
+++ Engine/Features/LevelLoader/LevelLoader.h
@@ -1,7 +1,18 @@
 #pragma once
 #include <string>
 
-#include <JsonParser/JsonLoader.h>
+#include <nlohmann/json.hpp>
+#include <memory>
+
+struct LevelData
+{
+    struct ObjectData
+    {
+        std::string filename = {};
+    };
+
+    std::vector<ObjectData> objects;
+};
 
 class LevelLoader
 {
@@ -14,5 +25,12 @@ private:
     const std::string extension_ = ".json";
     const std::string directory_ = "Resources/Level/";
 
+private:
+    using json = nlohmann::json;
+    std::unique_ptr<LevelData> levelData_ = nullptr;
 
+private:
+    void LoadJson(const std::string& _filename, json& _json);
+    void DeserializeJsonData(json& _json);
+    void DeserialiceMeshData(json& _object, LevelData::ObjectData& _objectData);
 };
