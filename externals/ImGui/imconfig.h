f81a4087ca641adf49763a7e101821c70b4373f1
Souto-Naitou <k023g0031@g.neec.ac.jp>
2025-10-02 16:45:24 +0900

    change: ファイル移動の影響で読み込めなかったパスを更新

--- externals/ImGui/imconfig.h
+++ externals/ImGui/imconfig.h
@@ -13,7 +13,7 @@
 //-----------------------------------------------------------------------------
 
 #pragma once
-#include "../VectorMatrix/math/Vector2.h"
+#include <Vector2.h>
 
 //---- Define assertion handler. Defaults to calling assert().
 // If your macro uses multiple statements, make sure is enclosed in a 'do { .. } while (0)' block so it can be used as a single statement.
