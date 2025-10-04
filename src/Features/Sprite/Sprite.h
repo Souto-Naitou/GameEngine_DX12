60a71608df1c2aa82980a1836e3bbd4964ee78cf
Souto-Naitou <k023g0031@g.neec.ac.jp>
2025-10-05 04:43:24 +0900

    update: Spriteクラスのデバッグ機能とメソッド修正
    * `Sprite.cpp`
      * デバッグ用のインクルードと`cmath`ライブラリを追加。
      * `Sprite::Update`メソッドで`isUpdate_`を`isUpdateEnabled_`に変更し、デバッグ時に`isUpdateCalled_`を設定。
      * `isFlipX`と`isFlipY`のチェックをそれぞれ`isFlipX_`と`isFlipY_`に変更。
      * UV変換行列の更新処理をコメントアウト。
      * `Sprite::Draw`メソッドで`isDraw_`を`isDrawEnabled_`に変更。
      * `SetSizeMultiply`メソッドを`SetSizeWithFactor`にリネーム。
      * テクスチャサイズ取得時に`float`型を`auto`に変更。
      * `ImGui`メソッドでチェックボックスを`isUpdateEnabled_`と`isDrawEnabled_`に変更し、更新未呼び出し時の警告メッセージを追加。
    
    * `Sprite.h`
      * `GetEnableUpdate`と`GetEnableDraw`メソッドをそれぞれ`isUpdateEnabled_`と`isDrawEnabled_`を返すように変更。
      * セッター関数を`isUpdateEnabled_`と`isDrawEnabled_`を使用するように変更。
      * `isUpdateCalled_`メンバ変数を追加し、更新処理の呼び出し管理を実装。
      * デバッグ用メンバ変数`aspectRatio_`と`thumbnailSize_`を追加。
    
    * その他
      * バイナリファイルやJSONの変更はありません。

--- src/Features/Sprite/Sprite.h
+++ src/Features/Sprite/Sprite.h
@@ -35,32 +35,32 @@ public:
 
 
 public: /// Getter
-    const bool          GetEnableUpdate()   const                   { return isUpdate_; }
-    const bool          GetEnableDraw()     const                   { return isDraw_; }
+    const bool          GetEnableUpdate()   const                   { return isUpdateEnabled_; }
+    const bool          GetEnableDraw()     const                   { return isDrawEnabled_; }
     const float         GetRotation()       const                   { return rotate_; }
     const Vector2&      GetPosition()       const                   { return translate_; }
     const Vector4&      GetColor()          const                   { return materialData_->color; }
     const Vector2&      GetSize()           const                   { return size_; }
     const Vector2&      GetAnchorPoint()    const                   { return anchorPoint_; }
-    const bool          GetFlipX()          const                   { return isFlipX; }
-    const bool          GetFlipY()          const                   { return isFlipY; }
+    const bool          GetFlipX()          const                   { return isFlipX_; }
+    const bool          GetFlipY()          const                   { return isFlipY_; }
     const Vector2&      GetTextureLeftTop() const                   { return textureLeftTop_; }
     const Vector2&      GetTextureSize()    const                   { return textureSize_; }
 
 public: /// Setter
     void                SetName(const std::string& name)            { pDebugEntry_->SetName(name); }
-    void                SetEnableUpdate(const bool _isUpdate)       { isUpdate_ = _isUpdate; }
-    void                SetEnableDraw(const bool _isDraw)           { isDraw_ = _isDraw; }
-    void                SetRotation(const float _rotation)          { rotate_ = _rotation; }
-    void                SetPosition(const Vector2& _position)       { translate_ = _position; }
-    void                SetColor(const Vector4& _color)             { materialData_->color = _color; }
-    void                SetSize(const Vector2& _size)               { size_ = _size; }
-    void                SetSizeMultiply(float _multiply);
-    void                SetAnchorPoint(const Vector2& _anchor)      { anchorPoint_ = _anchor; }
-    void                SetFlipX(const bool _isFlipX)               { isFlipX = _isFlipX; }
-    void                SetFlipY(const bool _isFlipY)               { isFlipY = _isFlipY; }
-    void                SetTextureLeftTop(const Vector2& _lt)       { textureLeftTop_ = _lt; }
-    void                SetTextureSize(const Vector2& _size)        { textureSize_ = _size; }
+    void                SetEnableUpdate(const bool isUpdate)       { isUpdateEnabled_ = isUpdate; }
+    void                SetEnableDraw(const bool isDraw)           { isDrawEnabled_ = isDraw; }
+    void                SetRotation(const float rotation)          { rotate_ = rotation; }
+    void                SetPosition(const Vector2& position)       { translate_ = position; }
+    void                SetColor(const Vector4& color)             { materialData_->color = color; }
+    void                SetSize(const Vector2& size)               { size_ = size; }
+    void                SetSizeWithFactor(float factor);
+    void                SetAnchorPoint(const Vector2& anchor)      { anchorPoint_ = anchor; }
+    void                SetFlipX(const bool isFlipX)               { isFlipX_ = isFlipX; }
+    void                SetFlipY(const bool isFlipY)               { isFlipY_ = isFlipY; }
+    void                SetTextureLeftTop(const Vector2& lt)       { textureLeftTop_ = lt; }
+    void                SetTextureSize(const Vector2& size)        { textureSize_ = size; }
 
 
 private: /// 他クラスが所持するインスタンスへのポインタ
@@ -71,16 +71,16 @@ private: /// 他クラスが所持するインスタンスへのポインタ
 
 private: /// メンバ変数
     std::unique_ptr<DebugEntry<Sprite>>         pDebugEntry_                    = nullptr;              // デバッグエントリ
-    bool                                        isUpdate_                       = true;                 // 有効かどうか。falseにするとUpdateとDrawをスキップする
-    bool                                        isDraw_                         = true;                 // 描画するかどうか。falseにするとDrawをスキップする
+    bool                                        isUpdateEnabled_                       = true;                 // 有効かどうか。falseにするとUpdateとDrawをスキップする
+    bool                                        isDrawEnabled_                         = true;                 // 描画するかどうか。falseにするとDrawをスキップする
 
     EulerTransform                              transform_                      = {};                   // 位置、回転、拡大縮小
     Vector2                                     size_                           = {};                   // サイズ
     float                                       rotate_                         = {};                   // 回転角
     Vector2                                     translate_                      = {};                   // 位置
     Vector2                                     anchorPoint_                    = {};                   // アンカーポイント
-    bool                                        isFlipX                         = false;                // X軸反転
-    bool                                        isFlipY                         = false;                // Y軸反転
+    bool                                        isFlipX_                        = false;                // X軸反転
+    bool                                        isFlipY_                        = false;                // Y軸反転
 
     Vector2                                     textureLeftTop_                 = {};                   // テクスチャの左上uv
     Vector2                                     textureSize_                    = {};                   // テクスチャのuv範囲
@@ -88,6 +88,9 @@ private: /// メンバ変数
     Matrix4x4                                   uvTransformMatrix_              = {};                   // UV変換行列
 
     std::string                                 texturePath_                    = {};                   // テクスチャファイルパス
+
+    /// ディスクリプタハンドル
+    D3D12_GPU_DESCRIPTOR_HANDLE                 textureSrvHandleGPU_            = {};                   // テクスチャハンドルGPU
 
     /// バッファリソース
     Microsoft::WRL::ComPtr<ID3D12Resource>      vertexResource_                 = nullptr;              // 頂点リソース
@@ -113,9 +116,11 @@ private: /// メンバ変数
     DirectX::TexMetadata                        metadata_                       = {};                   // テクスチャのメタデータ
 
     /// ImGui用
+    #ifdef _DEBUG
     float                                       aspectRatio_                    = 0.0f;                 // アスペクト比
     Vector2                                     thumbnailSize_                  = { 100.0f, 100.0f };   // サムネイルサイズ
-    D3D12_GPU_DESCRIPTOR_HANDLE                 textureSrvHandleGPU_            = {};                   // テクスチャハンドルGPU
+    bool                                        isUpdateCalled_                 = false;                // 更新処理が呼ばれたかどうか
+    #endif
 
 private: /// メンバ関数
     void CreateVertexResource();
